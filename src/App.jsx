3fd99c7b6a5a0e0ab754f00a5541cd04c2214808
argus1995 <ariefagus1995@gmail.com>
2025-06-18 15:08:16 +0700

    feat: add logo and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <div className="header-content">
+        <div className="logo">IR</div>
+        <h1>Indonesian Resto</h1>
+      </div>
+    </header>
+  );
 }
 
 export default App;
